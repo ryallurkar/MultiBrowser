726230e40d1013c88c9533cfffb077c50f189301
Yallurkar, Raju <raju90y@outlook.com>
2019-02-09 10:33:51 +0530

    formatted

--- src/tests/BaseTest.java
+++ src/tests/BaseTest.java
@@ -22,87 +22,81 @@ import utils.listeners.TestListener;
 
 @Listeners({ TestListener.class })
 public class BaseTest {
-    public WebDriver driver;
-    public  WebDriverWait wait;
-    ArrayList <String> results = new ArrayList <>();
-   	static String domain[] = {"gmail", "outlook"}; 
-   	static String names[] = {"ricky", "henry" , "rick", "jose", "shaun" , "smith" , "scott", "david"}; 
+	public WebDriver driver;
+	public WebDriverWait wait;
+	ArrayList<String> results = new ArrayList<>();
+	static String domain[] = { "gmail", "outlook" };
+	static String names[] = { "ricky", "henry", "rick", "jose", "shaun", "smith", "scott", "david" };
 
-    
-    public WebDriver getDriver() {
-        return driver;
-    }
-    
-    
-    @Parameters("browser")// this annotation is used to insert browser parameter from TestNG xml
-    @BeforeClass //The annotated method will be run before all tests in this suite have run
-    public void setup(@Optional("chrome") String browser) throws InterruptedException {
-    	
-    	System.setProperty("wdm.gitHubTokenName", "firefox_binary");
-    	System.setProperty("wdm.gitHubTokenSecret", "f2b0fed32144ea8feb5cd8cc7ed44f56d52c692f");
-        WebDriverManager.chromedriver().setup();
-        WebDriverManager.firefoxdriver().setup();
-        
-        if (browser.equalsIgnoreCase("Firefox"))
-        {
-        	driver = new FirefoxDriver();
-        } else if (browser.equalsIgnoreCase("chrome"))
-        	
-        {
-        	driver = new ChromeDriver();
-        }
-        else
-        {
-        	throw new IllegalArgumentException("The Browser Type is Undefined");
-        }
-        driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
-        //Create a wait. All test classes use this.
-        wait = new WebDriverWait(driver,30);
-    	driver.manage().window().maximize();
-	    driver.get("https://www.7tv.de");
-	    clickCookieButton();
-    }
-    
-    public void clickCookieButton() throws InterruptedException {
-    	WebElement cookieButton = driver.findElement(By.xpath("//a[@id=\"btn_cookie\" and @class=\"button-modal\"]"));
-    	try {
-    	   	if(cookieButton.isDisplayed()) {
-        		cookieButton.click();
-        		
-        	}
-    	   	}catch(Exception e){
-    	   		System.out.println("In Exception");
-    	    	JavascriptExecutor executor = (JavascriptExecutor)driver;
-    	    	executor.executeScript("arguments[0].click();", cookieButton);
-        	}
+	public WebDriver getDriver() {
+		return driver;
+	}
 
-    	}
-     
-    public void getAllTeasers() {
-    	List<WebElement> teasers = driver.findElements(By.cssSelector(".teaser-formatname"));
-    	for(WebElement element:teasers){
-    		results.add(element.getText());
-    	}
-    }
-    
-    public int getNumberOfTeasers() {
-    	return driver.findElements(By.cssSelector(".teaser-formatname")).size();
-    }
-    
-    public String getTeaserFrom(ArrayList<String> results, int index) {
+	@Parameters("browser") // this annotation is used to insert browser parameter from TestNG xml
+	@BeforeClass // The annotated method will be run before all tests in this suite have run
+	public void setup(@Optional("chrome") String browser) throws InterruptedException {
+
+		System.setProperty("wdm.gitHubTokenName", "firefox_binary");
+		System.setProperty("wdm.gitHubTokenSecret", "f2b0fed32144ea8feb5cd8cc7ed44f56d52c692f");
+		WebDriverManager.chromedriver().setup();
+		WebDriverManager.firefoxdriver().setup();
+
+		if (browser.equalsIgnoreCase("Firefox")) {
+			driver = new FirefoxDriver();
+		} else if (browser.equalsIgnoreCase("chrome"))
+
+		{
+			driver = new ChromeDriver();
+		} else {
+			throw new IllegalArgumentException("The Browser Type is Undefined");
+		}
+		driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
+		// Create a wait. All test classes use this.
+		wait = new WebDriverWait(driver, 30);
+		driver.manage().window().maximize();
+		driver.get("https://www.7tv.de");
+		clickCookieButton();
+	}
+
+	public void clickCookieButton() throws InterruptedException {
+		WebElement cookieButton = driver.findElement(By.xpath("//a[@id=\"btn_cookie\" and @class=\"button-modal\"]"));
+		try {
+			if (cookieButton.isDisplayed()) {
+				cookieButton.click();
+
+			}
+		} catch (Exception e) {
+			System.out.println("In Exception");
+			JavascriptExecutor executor = (JavascriptExecutor) driver;
+			executor.executeScript("arguments[0].click();", cookieButton);
+		}
+
+	}
+
+	public void getAllTeasers() {
+		List<WebElement> teasers = driver.findElements(By.cssSelector(".teaser-formatname"));
+		for (WebElement element : teasers) {
+			results.add(element.getText());
+		}
+	}
+
+	public int getNumberOfTeasers() {
+		return driver.findElements(By.cssSelector(".teaser-formatname")).size();
+	}
+
+	public String getTeaserFrom(ArrayList<String> results, int index) {
 		return results.get(index);
-    }
-    
-    public void searchTeaser(String teaserName) {
-    	driver.findElement(By.cssSelector(".site-search_input")).clear();
-	    driver.findElement(By.cssSelector(".site-search_input")).sendKeys(teaserName);
-	    driver.findElement(By.cssSelector(".site-search_submit.svg-lupe")).click();
-    }
-    
-    
-    @AfterClass(description = "Class Level Teardown!")
-    public void teardown () {
-        driver.quit();
-    }
-    
+	}
+
+	public void searchTeaser(String teaserName) {
+		driver.findElement(By.cssSelector(".site-search_input")).clear();
+		driver.findElement(By.cssSelector(".site-search_input")).sendKeys(teaserName);
+		driver.findElement(By.cssSelector(".site-search_submit.svg-lupe")).click();
+	}
+
+	@AfterClass(description = "Class Level Teardown!")
+	public void teardown() {
+		driver.quit();
+	}
+
 }
